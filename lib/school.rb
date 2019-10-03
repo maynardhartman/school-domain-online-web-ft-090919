9f4594e052d6b4b047e0f9d6896e1503e830c855
Maynard Hartman <mjhartman@brighthouse.com>
2019-10-03 02:31:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+class School
+  attr_accessor :name, :roster, :student_name, :grade
+  
+  def initialize(name)
+    @roster = Hash.new
+    @name = name
+    puts @name
+  end
+  
+  def add_student(student_name, student_grade)
+    puts "#{student_name} #{student_grade}"
+    if student_name == [] || student_grade == nil 
+      puts "Argument Error. No value or nil. Please retry."
+    else if @roster.key?(student_grade)  == true
+      @roster[student_grade] << student_name
+    else 
+      @roster[student_grade] = []
+      @roster[student_grade] << student_name
+    end
+  end
+
+  def grade(student_grade)
+    if student_grade == nil 
+      puts "Student Argument out of bounds. Please retry"
+    else if @roster.key?(student_grade) == true
+      return @roster[student_grade]
+    else 
+      puts "#{@roster[student_grade]} not found"
+    end
+  end
+end 
+def sort
+    return @roster.sort
+end
+end
+end
